ca4c63eb9c4df77703a67698a6a0dfdcf491552e
seitamuro <seitamuro0210@gmail.com>
2023-11-12 00:33:02 +0900

    当たり判定が残り続ける問題を解決

--- src/model/collisionController.ts
+++ src/model/collisionController.ts
@@ -14,11 +14,11 @@ export class CollisionController {
   constructor() {}
 
   public applyCollision() {
-    const bullets = Bullet.bullets.filter(
-      (elem) => !this._appliedBullet.includes(elem)
+    const deleted_bullets = this._appliedBullet.filter(
+      (elem) => elem.mesh.actionManager == null
     );
-    const enemies = Enemy.enemies.filter(
-      (elem) => !this._appliedEnemy.includes(elem)
+    const deleted_enemies = this._appliedEnemy.filter(
+      (elem) => elem.mesh.actionManager == null
     );
 
     this._appliedBullet = this._appliedBullet.filter((elem) =>
@@ -28,7 +28,38 @@ export class CollisionController {
       Enemy.enemies.includes(elem)
     );
 
-    for (const bullet of bullets) {
+    // 削除されたオブジェクトの当たり判定を削除する
+    for (const bullet of this._appliedBullet) {
+      for (const enemy of deleted_enemies) {
+        const action = bullet.mesh.actionManager.actions.filter(
+          (action) => action.getTriggerParameter().mesh == enemy.mesh
+        );
+        if (action.length > 0) {
+          bullet.mesh.actionManager.unregisterAction(action[0]);
+        }
+      }
+    }
+    for (const enemy of this._appliedEnemy) {
+      for (const bullet of deleted_bullets) {
+        const action = enemy.mesh.actionManager.actions.filter(
+          (action) => action.getTriggerParameter().mesh == bullet.mesh
+        );
+        if (action.length > 0) {
+          enemy.mesh.actionManager.unregisterAction(action[0]);
+        }
+      }
+    }
+
+    // 新しく追加されたオブジェクトを取得する
+    const new_bullets = Bullet.bullets.filter(
+      (elem) => !this._appliedBullet.includes(elem)
+    );
+    const new_enemies = Enemy.enemies.filter(
+      (elem) => !this._appliedEnemy.includes(elem)
+    );
+
+    // 新しく追加されたオブジェクトに既存のオブジェクトの当たり判定を適用する
+    for (const bullet of new_bullets) {
       for (const enemy of this._appliedEnemy) {
         bullet.mesh.actionManager.registerAction(
           new BABYLON.ExecuteCodeAction(
@@ -43,24 +74,7 @@ export class CollisionController {
         );
       }
     }
-
-    for (const bullet of this._appliedBullet) {
-      for (const enemy of enemies) {
-        bullet.mesh.actionManager.registerAction(
-          new BABYLON.ExecuteCodeAction(
-            {
-              trigger: BABYLON.ActionManager.OnIntersectionEnterTrigger,
-              parameter: {
-                mesh: enemy.mesh,
-              },
-            },
-            () => bullet.collisionHandler(enemy)
-          )
-        );
-      }
-    }
-
-    for (const enemy of enemies) {
+    for (const enemy of new_enemies) {
       for (const bullet of this._appliedBullet) {
         enemy.mesh.actionManager.registerAction(
           new BABYLON.ExecuteCodeAction(
@@ -76,8 +90,24 @@ export class CollisionController {
       }
     }
 
+    // 既存のオブジェクトに新しく追加されたオブジェクトの当たり判定を適用する
+    for (const bullet of this._appliedBullet) {
+      for (const enemy of new_enemies) {
+        bullet.mesh.actionManager.registerAction(
+          new BABYLON.ExecuteCodeAction(
+            {
+              trigger: BABYLON.ActionManager.OnIntersectionEnterTrigger,
+              parameter: {
+                mesh: enemy.mesh,
+              },
+            },
+            () => bullet.collisionHandler(enemy)
+          )
+        );
+      }
+    }
     for (const enemy of this._appliedEnemy) {
-      for (const bullet of bullets) {
+      for (const bullet of new_bullets) {
         enemy.mesh.actionManager.registerAction(
           new BABYLON.ExecuteCodeAction(
             {
@@ -92,7 +122,7 @@ export class CollisionController {
       }
     }
 
-    this._appliedBullet = [...this._appliedBullet, ...bullets];
-    this._appliedEnemy = [...this._appliedEnemy, ...enemies];
+    this._appliedBullet = [...this._appliedBullet, ...new_bullets];
+    this._appliedEnemy = [...this._appliedEnemy, ...new_enemies];
   }
 }
