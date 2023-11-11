9c3c0543bd35aadaabcae88b4596d76cc1497f59
seitamuro <seitamuro0210@gmail.com>
2023-11-11 13:03:43 +0900

    プレイヤーの移動を実装

--- src/model/player.ts
+++ src/model/player.ts
@@ -1,11 +1,31 @@
 import * as BABYLON from "@babylonjs/core";
 
 import { StandardMaterialBox } from "../types/StandardMaterialBox";
+import { getRelativeDirection } from "../utils/getRelativeDirection";
+
+enum Key {
+  W,
+  A,
+  S,
+  D,
+  Q,
+  E,
+}
 
 export class Player {
   mesh: StandardMaterialBox;
+  pressed_key: Set<Key>;
+  engine: BABYLON.Engine;
+  scene: BABYLON.Scene;
+  move_speed: number = 0.5;
+  rotate_speed: number = 0.2;
 
   constructor(scene: BABYLON.Scene, engine: BABYLON.Engine) {
+    this.pressed_key = new Set();
+    this.scene = scene;
+    this.engine = engine;
+
+    // create mesh
     this.mesh = BABYLON.MeshBuilder.CreateBox(
       "player",
       {
@@ -18,5 +38,98 @@ export class Player {
     this.mesh.material = new BABYLON.StandardMaterial("mat");
     this.mesh.material.diffuseColor = BABYLON.Color3.Yellow();
     this.mesh.position = new BABYLON.Vector3(0, 0, -2);
+
+    // event listener
+    scene.onKeyboardObservable.add(this._keyboard_event_callback);
+    scene.onBeforeRenderObservable.add(this._before_render_callback);
   }
+
+  // Event Callbacks
+  private _keyboard_event_callback = (kbInfo: BABYLON.KeyboardInfo) => {
+    switch (kbInfo.type) {
+      case BABYLON.KeyboardEventTypes.KEYDOWN:
+        switch (kbInfo.event.key) {
+          case "w":
+            this.pressed_key.add(Key.W);
+            break;
+          case "a":
+            this.pressed_key.add(Key.A);
+            break;
+          case "s":
+            this.pressed_key.add(Key.S);
+            break;
+          case "d":
+            this.pressed_key.add(Key.D);
+            break;
+          case "q":
+            this.pressed_key.add(Key.Q);
+            break;
+          case "e":
+            this.pressed_key.add(Key.E);
+            break;
+        }
+        break;
+      case BABYLON.KeyboardEventTypes.KEYUP:
+        switch (kbInfo.event.key) {
+          case "w":
+            this.pressed_key.delete(Key.W);
+            break;
+          case "a":
+            this.pressed_key.delete(Key.A);
+            break;
+          case "s":
+            this.pressed_key.delete(Key.S);
+            break;
+          case "d":
+            this.pressed_key.delete(Key.D);
+            break;
+          case "q":
+            this.pressed_key.delete(Key.Q);
+            break;
+          case "e":
+            this.pressed_key.delete(Key.E);
+            break;
+        }
+        break;
+    }
+  };
+
+  private _before_render_callback = () => {
+    const delta = this.engine.getDeltaTime() / 60;
+    this._player_move(delta);
+    this._player_rotate(delta);
+  };
+
+  private _player_move = (delta: number) => {
+    const direction = new BABYLON.Vector3(0, 0, 0);
+    if (this.pressed_key.has(Key.W)) {
+      direction.addInPlace(new BABYLON.Vector3(0, 0, 1));
+    }
+    if (this.pressed_key.has(Key.A)) {
+      direction.addInPlace(new BABYLON.Vector3(-1, 0, 0));
+    }
+    if (this.pressed_key.has(Key.S)) {
+      direction.addInPlace(new BABYLON.Vector3(0, 0, -1));
+    }
+    if (this.pressed_key.has(Key.D)) {
+      direction.addInPlace(new BABYLON.Vector3(1, 0, 0));
+    }
+
+    const normalized_direction = direction.normalize();
+    this.mesh.position.addInPlace(
+      getRelativeDirection(
+        normalized_direction,
+        this.mesh.rotation
+      ).scaleInPlace(delta * this.move_speed)
+    );
+  };
+
+  private _player_rotate = (delta: number) => {
+    if (this.pressed_key.has(Key.Q)) {
+      this.mesh.rotation.y -= delta * this.rotate_speed;
+    }
+    if (this.pressed_key.has(Key.E)) {
+      this.mesh.rotation.y += delta * this.rotate_speed;
+    }
+  };
 }
