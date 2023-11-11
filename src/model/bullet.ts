1a6f9e859ece7f97330e9ca6008b4cca57dcf13c
seitamuro <seitamuro0210@gmail.com>
2023-11-11 13:31:32 +0900

    Bulletが自動で移動するようにした

--- src/model/bullet.ts
+++ src/model/bullet.ts
@@ -1,11 +1,13 @@
 import * as BABYLON from "@babylonjs/core";
 
 import { StandardMaterialSphere } from "../types";
+import { getRelativeDirection } from "../utils/getRelativeDirection";
 
 export class Bullet {
   mesh: StandardMaterialSphere;
   scene: BABYLON.Scene;
   engine: BABYLON.Engine;
+  speed: number = 1;
 
   constructor(scene: BABYLON.Scene, engine: BABYLON.Engine) {
     this.scene = scene;
@@ -22,5 +24,19 @@ export class Bullet {
     ) as StandardMaterialSphere;
     this.mesh.material = new BABYLON.StandardMaterial("bullet", scene);
     this.mesh.material.diffuseColor = BABYLON.Color3.Yellow();
+
+    this.scene.onBeforeRenderObservable.add(() => {
+      this._move(this.engine.getDeltaTime() / 60);
+    });
   }
+
+  private _move = (delta: number) => {
+    const normalized_direction = new BABYLON.Vector3(0, 0, 1);
+    this.mesh.position.addInPlace(
+      getRelativeDirection(
+        normalized_direction,
+        this.mesh.rotation
+      ).scaleInPlace(delta * this.speed)
+    );
+  };
 }
